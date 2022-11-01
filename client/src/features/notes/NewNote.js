750be6e432a90edc0803211d5ca96b9b45789094
Dagmar Lewis <dagmarlewis8@gmail.com>
2022-11-01 09:34:40 -0500

    Completed frontend  auth

--- client/src/features/notes/NewNote.js
+++ client/src/features/notes/NewNote.js
@@ -4,8 +4,10 @@ import NewNoteForm from './NewNoteForm'
 
 const NewNote = () => {
     const users = useSelector(selectAllUsers)
+    
+    if (!users?.length) return <p>Not Currently Available</p>
 
-    const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+    const content =  <NewNoteForm users={users} /> 
 
     return content
 }
